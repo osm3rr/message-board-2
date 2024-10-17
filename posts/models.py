a655ee5d67ab49843d86cc5640001ce1eb1e0798
osm3rr <osm3rr@gmail.com>
2024-10-17 16:41:31 -0400

    post-detail

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Publication(models.Model):
+    title = models.CharField(max_length=200, default="",)
     text = models.TextField()
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+        default="",
+    )
 
     def __str__(self) -> str:
-        return self.text[:50]
+        return self.title
